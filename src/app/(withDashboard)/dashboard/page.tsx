656c57f2edbc82fa7b1fd6ce8be512cce400b41b
Anamul Haque <anamulhaque9901@gmail.com>
2024-09-10 21:38:03 +0600

    Cleaned up commit without secrets

--- src/app/(withDashboard)/dashboard/page.tsx
+++ src/app/(withDashboard)/dashboard/page.tsx
@@ -1,7 +1,34 @@
-const DashboardPage = () => {
+import { authOptions } from "@/utils/authOptions";
+import { getServerSession } from "next-auth";
+import Image from "next/image";
+
+const DashboardPage = async () => {
+  //* auth signIn er por user info cookies e 'next-auth-session-token' e store thake. ekhon ai store kora user info get korbo
+
+  const session = await getServerSession(authOptions);
+  console.log(session);
   return (
     <div>
-      <h1 className="text-4xl text-center mt-10">Welcome To Dashboard Page</h1>
+      {session?.user && (
+        <>
+          <h1 className="text-4xl text-center mt-10">
+            Welcome {session?.user?.name}
+          </h1>
+          <h1 className="text-4xl text-center mt-10">
+            Looged-in user email: {session?.user?.email}
+          </h1>
+          <Image
+            src={
+              session?.user?.image ||
+              "https://i.ibb.co.com/4jnBKKw/pngwing-com-45.png"
+            }
+            alt="user image"
+            width={150}
+            height={150}
+            className="mx-auto rounded-full mt-10"
+          />
+        </>
+      )}
     </div>
   );
 };
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { signIn } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
 import { useForm } from "react-hook-form";
@@ -77,7 +78,15 @@ const LoginPage = () => {
           </form>
           <p className="text-center">Or Sign Up Using</p>
           <div className="flex justify-center mb-10 mt-2">
-            <button className="btn btn-circle ">
+            <button
+              className="btn btn-circle"
+              //* google auth account e 'Authorized redirect URIs' ai option e 'http://localhost:3000/api/auth/callback/google' URL use korbo
+              onClick={() =>
+                signIn("google", {
+                  callbackUrl: "http://localhost:3000/dashboard",
+                })
+              }
+            >
               <Image
                 src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-webinar-optimizing-for-success-google-business-webinar-13.png"
                 width={50}
@@ -85,7 +94,15 @@ const LoginPage = () => {
                 alt="google logo"
               />
             </button>
-            <button className="btn btn-circle">
+            {/* auth: signIn("what method", {callbackUrl: "which page rediract after signIn"}) */}
+            <button
+              className="btn btn-circle"
+              onClick={() =>
+                signIn("github", {
+                  callbackUrl: "http://localhost:3000/dashboard",
+                })
+              }
+            >
               <Image
                 src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
                 width={35}
